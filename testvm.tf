981381e755aa0590766c7f43dc16e1ff454ee28f
Dave Hall <davemhall@gmail.com>
2016-11-02 21:40:32 +0000

    fix public ip

--- testvm.tf
+++ testvm.tf
@@ -20,6 +20,7 @@ resource "azurerm_network_interface" "testnic" {
         subnet_id = "${azurerm_subnet.pipelineSubnet1.id}"
         private_ip_address_allocation = "dynamic"
         public_ip_address_id = "${azurerm_public_ip.testPublicIp.id}"
+        
     }
 
 }
